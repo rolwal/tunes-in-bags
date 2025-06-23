aa3d152e11dfa67889a9832916a00f1df4629ac9
Ada Mantic <ada@cantor.connect.lan>
2025-06-23 19:32:23 +0100

    build.sh: add htop, psacct; remove tmux, btop

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,8 +9,11 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+# this installs packages from fedora repos
+dnf5 install -y htop psacct
+
+# this removes already installed and configured packages
+dnf5 remove -y btop
 
 # Use a COPR Example:
 #
